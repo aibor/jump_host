522c0b9bb650a8c12830961b85a155c2b2a72352
Tobias Böhm <code@aibor.de>
2015-11-14 19:50:35 +0100

    added no warranty, as it may blow up your laptop

--- jump_host.rb
+++ jump_host.rb
@@ -1,7 +1,10 @@
 # Copyright (C) 2015 aibo <aibo+code@aibor.de>
-# This work is free. You can redistribute it and/or modify it under the
-# terms of the Do What The Fuck You Want To Public License, Version 2,
-# as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+#
+# This work is free. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://www.wtfpl.net/ for more details.
 #
 # 
 # Usage:
