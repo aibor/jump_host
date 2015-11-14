98bd51dbf135888fd0310a9b794c282c2f3aa86f
Tobias Böhm <code@aibor.de>
2015-11-14 19:27:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# JumpHost module
+
+## Why?
+
+If you need temporary droplets in different locations.
+
+## How?
+
+This module is based on the `droplet_kit` gem.
+
+You'll need a snapshot at DO that will be used as a base. You might
+specify a particular one. If you don't, the first is taken.
+
+Also you need SSH Keys set up, so you don't need to hassle about the
+root password.
+
+Third thing you need is an API key that has write permission at DO.
+
+In order to deploy and drop droplets easily via cli, a bash function
+can be added to you shell rc. Make sure to replace the variable values
+to match your needs.
+
+```bash
+# .bashrc
+jump_host() {
+  ruby -I$HOME/Projekte/ruby/jump_host -rjump_host <<-EOC
+  
+  # format string for the hostname, %s is replaced with the region
+  JumpHost::Droplet.name_format = "jump-%s.your-do.host"
+
+  # replace with your API key
+  JumpHost::DOApi.token =
+  '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef'
+
+  # optional, first snapshot taken if this is not set
+  JumpHost::Droplet.default_image_name = "my_jump_host_image"
+
+  puts JumpHost::Droplet.send("$1", "$2")
+EOC
+}
+```
+
+Need a droplet in New York? Create it
+```bash
+$ jump_host deploy nyc1
+new
+1.2.3.4
+```
+
+Forgot the IP? Show it again
+```bash
+$ jump_host show nyc1
+new
+1.2.3.4
+```
+
+Work done? Drop it
+```bash
+$ jump_host drop nyc1
+true
+```
+
+If something goew wrong, you'll see more or less helpful Ruby exceptions.
+
+If you don't like or need such a simple bash function, take a look at
+the code and use it in your ruby scripts.
---
+++ jump_host.rb
@@ -0,0 +1,163 @@
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO. 
+#
+# 
+# Usage:
+#   require 'jump_host'
+#
+#   # format string for the hostname, %s is replaced with the region
+#   JumpHost::Droplet.name_format = "jump-%s.your-do.host"
+#
+#   # replace with your API key
+#   JumpHost::DOApi.token =
+#     '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef'
+#
+#   # optional, first snapshot taken if this is not set
+#   JumpHost::Droplet.default_image_name = "my_jump_host_image"
+#
+#   JumpHost::DOApi.images.map &:name
+#
+
+require 'droplet_kit'
+
+module JumpHost
+  class Error < ::StandardError; end
+
+  module DOApi
+    include ::DropletKit
+
+    class TokenNotSetError < Error; end
+
+    class << self
+      attr_writer :token
+    end
+
+    module_function 
+
+    def client(refresh: true)
+      if @token
+        @client = nil if refresh
+        @client ||= Client.new(access_token: @token)
+      else
+        raise TokenNotSetError
+      end
+    end
+
+    def images(refresh: false)
+      @images = nil if refresh
+      @images ||= client.images.all.select { |i| i.public == false }
+    end
+
+    def droplets(refresh: false)
+      @droplets = nil if refresh
+      @droplets ||= client.droplets.all
+    end
+
+    def ssh_keys(refresh: false)
+      @ssh_keys = nil if refresh
+      @ssh_keys ||= client.ssh_keys.all
+    end
+  end
+
+  module Droplet
+    extend self
+
+    class ExistsError < Error; end
+    class NotDeployedError < Error; end
+    class NameFormatNotSetError < Error; end
+    class NoImageFoundError < Error; end
+
+    class << self
+      attr_writer :name_format, :default_image_name
+    end
+
+    def deploy(region, image_name: nil)
+      if exists_at? region
+        raise ExistsError
+      elsif not image = find_image(image_name)
+        raise NoImageFoundError
+      else
+        droplet = new region, image
+        DOApi.client.droplets.create droplet
+        show region
+      end
+    end
+
+    def show(region)
+      if droplet = find_at(region, refresh: true)
+        return droplet.status, droplet.public_ip
+      else
+        raise NotDeployedError
+      end
+    end
+
+    def drop(region)
+      if droplet = find_at(region)
+        DOApi.client.droplets.delete id: droplet.id
+      else
+        raise NotDeployedError
+      end
+    end
+
+    def name(region)
+      if @name_format
+        @name_format % region
+      else
+        raise NameFormatNotSetError
+      end
+    end
+    private :name
+
+    def new(region, image)
+      ssh_key_ids = DOApi.ssh_keys.map &:id
+      options = {
+        name: name(region),
+        region: region,
+        image: image.id,
+        size: '512mb',
+        ssh_keys: ssh_key_ids
+      }
+
+      ::DropletKit::Droplet.new options
+    end
+    private :new
+
+    def find_at(region, refresh: false)
+      DOApi.droplets(refresh: refresh).find do |d|
+        d.name == name(region)
+      end
+    end
+
+    def exists_at?(region, refresh: false)
+      !!find_at(region, refresh: refresh)
+    end
+    private :exists_at?
+
+    def find_image(name)
+      images = DOApi.images
+      find = lambda { |name| images.find { |i| i.name == name } }
+
+      if name
+        find.(name)
+      elsif @default_image_name
+        find.(@default_image_name)
+      else
+        images.first
+      end
+    end
+    private :find_image
+  end
+
+  extend Droplet 
+end
